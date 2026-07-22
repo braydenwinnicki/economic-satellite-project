a9c90f6a7925192da96e73e8db8a867ecd1ffbef
braydenwinnicki <bw.141800@gmail.com>
2026-07-21 20:42:01 -0400

    added l3 model and trainer

---
+++ models/resnet_unfrozen_l3.py
@@ -0,0 +1,48 @@
+from torchvision.models import resnet18, ResNet18_Weights
+import torch.nn as nn
+
+
+class ResNetRegressorUnrozen(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+
+        self.weights = ResNet18_Weights.DEFAULT
+
+        self.model = resnet18(weights=self.weights)
+
+        #freeeze all
+        for param in self.model.parameters():
+            param.requires_grad = False
+        #unfreze l3
+        for param in model.resnet.layer3.parameters():
+            param.requires_grad = True
+        #unfreeze l4
+        for param in model.resnet.layer4.parameters():
+            param.requires_grad = True
+
+        # replace classifier
+        self.model.fc = nn.Linear(self.model.fc.in_features, 1)
+
+    def forward(self, x, mask):
+
+        B, T, C, H, W = x.shape
+
+        x = x.view(B * T, C, H, W)
+
+        x = self.model(x)
+
+        x = x.view(B, T, 1)
+
+        # ensure mask is float for multiplication
+        mask = mask.unsqueeze(-1).float()
+
+        x = x * mask
+
+        prediction = x.sum(dim=1) / mask.sum(dim=1).clamp(min=1)
+
+        return prediction
+
+
+if __name__ == "__main__":
+    print(ResNetRegressor())
